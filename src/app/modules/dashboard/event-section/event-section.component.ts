9fa03c8487948daf127f31b053cae7ddf973bb3e
Jith <jith4j@gmail.com>
2024-06-23 21:29:43 +0800

    Added components for each section including scss

---
+++ src/app/modules/dashboard/event-section/event-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './event-section.component.html',
+  styleUrl: './event-section.component.scss'
+})
+export class EventSectionComponent {
+
+}
--- src/app/modules/dashboard/layout/landing/landing.component.ts
+++ src/app/modules/dashboard/layout/landing/landing.component.ts
@@ -3,11 +3,14 @@ import { FooterComponent } from '../footer/footer.component';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from '../home/home.component';
+import { AboutUsComponent } from '../../about-us/about-us.component';
+import { EventSectionComponent } from '../../event-section/event-section.component';
+import { ContactUsComponent } from '../../contact-us/contact-us.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [CommonModule, RouterModule,FooterComponent,HomeComponent],
+  imports: [CommonModule, RouterModule,FooterComponent,HomeComponent, AboutUsComponent, EventSectionComponent, ContactUsComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
