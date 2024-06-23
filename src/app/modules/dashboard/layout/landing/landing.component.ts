435f107f69069bfcac380e3756578382f7276af9
pujaanil <pujaanil007@gmail.com>
2024-06-23 18:34:05 +0530

    Added new componets Contact Us and Home

--- src/app/modules/dashboard/layout/landing/landing.component.ts
+++ src/app/modules/dashboard/layout/landing/landing.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from '../home/home.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [CommonModule, RouterModule,FooterComponent],
+  imports: [CommonModule, RouterModule,FooterComponent,HomeComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
