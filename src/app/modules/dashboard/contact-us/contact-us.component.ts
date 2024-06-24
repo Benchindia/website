0a81c024ab13da0a9fb941535f1059dde63de0b1
Jith <jith4j@gmail.com>
2024-06-24 12:10:44 +0800

    added parallax, contact-us page, content creation.

--- src/app/modules/dashboard/contact-us/contact-us.component.ts
+++ src/app/modules/dashboard/contact-us/contact-us.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
@@ -8,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrl: './contact-us.component.scss'
 })
 export class ContactUsComponent {
+  contactForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log('Form Submitted!', this.contactForm.value);
+      // Handle form submission, e.g., send the data to the server
+    }
+  }
 }
