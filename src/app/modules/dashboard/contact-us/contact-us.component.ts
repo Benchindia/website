cdc78dc334ff56224402e1232835466520738cc7
pujaanil <pujaanil007@gmail.com>
2024-06-24 11:54:04 +0530

    Contact-Us Form Fixed

--- src/app/modules/dashboard/contact-us/contact-us.component.ts
+++ src/app/modules/dashboard/contact-us/contact-us.component.ts
@@ -1,32 +1,41 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule,FormsModule, ReactiveFormsModule],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.scss'
 })
 export class ContactUsComponent {
-  contactForm: FormGroup;
+  baseForm!: FormGroup;
 
   constructor(private fb: FormBuilder) {
-    this.contactForm = this.fb.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      message: ['', Validators.required]
+    this.formInIt()
+  }
+
+  formInIt(): any {
+    this.baseForm = this.fb.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      email: ['', [Validators.required,Validators.email]],
+      message: [''],
     });
   }
 
-  onSubmit() {
-    console.log(this.contactForm)
-    if (this.contactForm.valid) {
-      console.log('Form Submitted!', this.contactForm.value);
-      // Handle form submission, e.g., send the data to the server
+  isFieldInvalid(field: string): boolean {
+    const control = this.baseForm.get(field);
+    return control ? control.invalid && (control.dirty || control.touched) : false;
+  }
+
+  formSubmit(){
+    if(this.baseForm.invalid) {
+      this.baseForm.markAllAsTouched()
+      return
     }
+    console.log(this.baseForm.value)
   }
 }
