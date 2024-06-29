21cc32d6689dbd68e7713066fec0ae36c459e2f6
Jith <jith4j@gmail.com>
2024-06-29 21:37:47 +0800

    design changes - background blur, font

--- src/app/modules/dashboard/event-section/event-section.component.ts
+++ src/app/modules/dashboard/event-section/event-section.component.ts
@@ -11,37 +11,27 @@ import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
   styleUrls: ['./event-section.component.scss']
 })
 export class EventSectionComponent {
-  objective: boolean = true;
+  about: boolean = true;
+  objective: boolean = false;
   criteria: boolean = false;
-  weOffer: boolean = false;
-  weExpect: boolean = false;
 
   itemClick(event: any, value: any) {
     if (event) {
       switch (value) {
+        case 'Abt':
+          this.about = true;
+          this.objective = false;
+          this.criteria = false;
+          break;
         case 'Obj':
+          this.about = false;
           this.objective = true;
           this.criteria = false;
-          this.weOffer = false;
-          this.weExpect = false;
           break;
         case 'Cri':
+          this.about = false;
           this.objective = false;
           this.criteria = true;
-          this.weOffer = false;
-          this.weExpect = false;
-          break;
-        case 'Offer':
-          this.objective = false;
-          this.criteria = false;
-          this.weOffer = true;
-          this.weExpect = false;
-          break;
-        case 'Expect':
-          this.objective = false;
-          this.criteria = false;
-          this.weOffer = false;
-          this.weExpect = true;
           break;
       }
     }
