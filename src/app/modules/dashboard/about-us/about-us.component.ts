d7bcc80dd111ea9bf1df9771e76811b26def3196
pujaanil <pujaanil007@gmail.com>
2024-07-02 21:49:57 +0530

    Changes made to about us section

--- src/app/modules/dashboard/about-us/about-us.component.ts
+++ src/app/modules/dashboard/about-us/about-us.component.ts
@@ -8,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrl: './about-us.component.scss'
 })
 export class AboutUsComponent {
+  about: boolean = true;
+  objective: boolean = false;
+  criteria: boolean = false;
+
+  itemClick(event: any, value: any) {
+    if (event) {
+      switch (value) {
+        case 'Abt':
+          this.about = true;
+          this.objective = false;
+          this.criteria = false;
+          break;
+        case 'Obj':
+          this.about = false;
+          this.objective = true;
+          this.criteria = false;
+          break;
+        case 'Cri':
+          this.about = false;
+          this.objective = false;
+          this.criteria = true;
+          break;
+      }
+    }
+  }
 
 }
