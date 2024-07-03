1e24016054ae714021958e4c90c92d85f510fa10
Jith <jith4j@gmail.com>
2024-07-03 23:42:09 +0800

    Email integration done using email js

--- src/app/modules/dashboard/contact-us/contact-us.component.ts
+++ src/app/modules/dashboard/contact-us/contact-us.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import emailjs from 'emailjs-com';
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [CommonModule, RouterModule,FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.scss'
 })
@@ -22,7 +23,7 @@ export class ContactUsComponent {
       firstName: ['', [Validators.required]],
       lastName: ['', [Validators.required]],
       email: ['', [Validators.required,Validators.email]],
-      message: [''],
+      message: ['', [Validators.required]],
     });
   }
 
@@ -37,5 +38,21 @@ export class ContactUsComponent {
       return
     }
     console.log(this.baseForm.value)
+    const templateParams = {
+      firstName: this.baseForm.value.firstName,
+      lastName: this.baseForm.value.lastName,
+      email: this.baseForm.value.email,
+      message: this.baseForm.value.message,
+    };
+
+    emailjs.send('service_bench', 'template_ul6sy2f', templateParams, 'MPz4x7-DheEnqC1-6')
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+        alert('Your message has been sent successfully!');
+        this.baseForm.reset();
+      }, (error) => {
+        console.log('FAILED...', error);
+        alert('There was an error sending your message. Please try again.');
+      });
   }
 }
